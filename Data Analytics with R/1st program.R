714a84734ce23fefe2cef0daa483e840d71ef490
Bhavyachowdaraj <149554324+Bhavyachowdaraj@users.noreply.github.com>
2024-02-19 16:24:53 +0530

    program 4

---
+++ Data Analytics with R/1st program.R
@@ -0,0 +1,19 @@
+#Double
+double_var <- 3.14
+cat("Double Variable:",double_var,"\tType:",typeof(double_var),"\n")
+
+#Integer 
+int_var<-42L
+cat("integer Variable:",int_var,"\t Type:",typeof(int_var),"\n")
+
+#Logical
+logical_var<-TRUE
+cat("Logical Variable:",logical_var,"\t Type:",typeof(logical_var),"\n")
+
+#Complex
+complex_var<-1+2i
+cat("Complex Variable:",complex_var,"\t Type:",typeof(complex_var),"\n")
+
+#Character
+character_var<-"Hello,R!"
+cat("Character Variable:",character_var,"\t Type:",typeof(character_var),"\n")
